54f8155d58a9cbfdfeac7cae5d4ac30bfd290dc0
Joshua Bernard <joshualeebernard.dev@gmail.com>
2024-12-07 08:27:35 +0000

    Added SVG for seach Icon in search bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,14 +3,37 @@ import styles from './SearchBar.module.css';
 
 export default function SearchBar() {
   const [search, setSearch] = useState('');
+
   function handleUserInput(e) {
     setSearch(e.target.value);
-
   }
+
   return (
     <>
-    <h2 className={styles.searchTitle}>Search</h2>
-      <input className={styles.searchInput} id='search' type="text" onChange={handleUserInput} value={search}/>
+      <h2 className={styles.searchTitle}>Search</h2>
+      <div className={styles.searchInputContainer}>
+        <input 
+          className={styles.searchInput} 
+          id='search' 
+          type="text" 
+          onChange={handleUserInput} 
+          value={search} 
+        />
+        <svg 
+          className={styles.searchIcon}
+          width="29" 
+          height="29" 
+          viewBox="0 0 29 29" 
+          fill="none" 
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path 
+            d="M17.5061 17.364L28.1127 27.9706M18.2132 3.92893C22.1184 7.83418 22.1184 14.1658 18.2132 18.0711C14.308 21.9763 7.97631 21.9763 4.07107 18.0711C0.165825 14.1658 0.165825 7.83418 4.07107 3.92893C7.97631 0.0236893 14.308 0.0236893 18.2132 3.92893Z" 
+            stroke="#40DD8F" 
+            strokeWidth="2"
+          />
+        </svg>
+      </div>
     </>
   );
 }
