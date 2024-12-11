b990a88bb98367b933a2f82f5293444d817d6394
joshualeebernard.dev <joshualeebernard.dev@gmail.com>
2024-12-11 10:13:34 +0000

    Added track add/remove input to Track Component

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -1,10 +1,18 @@
 import PropTypes from 'prop-types';
+import styles from './Track.module.css';
 export default function Track({ song }) {
   return (
     <>
-      <div className="track">
-        <p>{song.name}</p>
-        <p>{song.artist}</p>
+      <div className={styles.Track}>
+        <div className={styles.TrackInfo}>
+          <p>{song.name}</p>
+          <p>{song.artist}</p>
+        </div>
+        <div className={styles.trackButtonContainer}>
+          <input className={styles.trackButton} type="button" value={song.isInPlaylist ? "-" : "+"} />
+
+        </div>
+        <hr></hr>
       </div>
     </>
   );
@@ -15,6 +23,7 @@ Track.propTypes = {
     name: PropTypes.string.isRequired,
     artist: PropTypes.string.isRequired,
     album: PropTypes.string,
+    isInPlaylist: PropTypes.bool,
     id: PropTypes.number,
   }).isRequired,
 };
