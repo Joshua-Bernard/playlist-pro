8d1e2e5e6a6aadc022480d09146c5a3ed43009dd
Joshua Bernard <joshualeebernard.dev@gmail.com>
2025-07-26 09:33:10 +0100

    Updated scope

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -42,7 +42,7 @@ const Spotify = {
     params.append('client_id', clientId);
     params.append('response_type', 'code');
     params.append('redirect_uri', redirectUri);
-    params.append('scope', 'user-read-private user-read-email');
+    params.append('scope', 'user-read-private user-read-email playlist-modify-private playlist-modify-public');
     params.append('code_challenge_method', 'S256');
     params.append('code_challenge', challenge);
 
